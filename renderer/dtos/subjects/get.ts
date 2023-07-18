670876d8ccd2cf58a59f39a190f984b939989a56
Gang Yi <khorgangyi@gmail.com>
2023-07-19 01:09:25 +0800

    Package Discount Page - Remove subjects dtos and response

--- renderer/dtos/subjects/get.ts
+++
@@ -1,7 +0,0 @@
-export type SubjectsGetDto = {
-	id?: number;
-	form_id?: number;
-	subject_name?: string;
-	is_active?: boolean;
-	orderBy?: string;
-};
--- renderer/responses/subjects/create.ts
+++
@@ -1,6 +0,0 @@
-export type SubjectsCreateResponse = {
-	id: number;
-	form_id: number;
-	subject_name: string;
-	is_active: boolean;
-};
