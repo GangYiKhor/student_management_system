eeb40e0a8a002fcdaf5941fc81711a5046cc3f85
Gang Yi <khorgangyi@gmail.com>
2024-09-10 20:16:44 +0100

    Updated receipt db to unlink from student and class tables

--- db/create_table.sql
+++ db/create_table.sql
@@ -85,7 +85,7 @@ CREATE TABLE "voucher" (
 	"id" VARCHAR(255) PRIMARY KEY,
 	"student_id" INT,
 	"discount" REAL NOT NULL,
-	"is_percentage" BOOLEAN NOT NULL DEFAULT FALSE,
+	"is_percentage" BOOLEAN NOT NULL,
 	"start_date" DATE NOT NULL DEFAULT CURRENT_DATE,
 	"expired_at" DATE NOT NULL,
 	"used" BOOLEAN NOT NULL DEFAULT FALSE,
@@ -95,6 +95,9 @@ CREATE TABLE "voucher" (
 CREATE TABLE "receipt" (
 	"id" SERIAL PRIMARY KEY,
 	"student_id" INT NOT NULL,
+	"student_name" VARCHAR(255) NOT NULL,
+	"form_id" INT NOT NULL,
+	"form_name" VARCHAR(255) NOT NULL,
 	"date" DATE NOT NULL DEFAULT CURRENT_DATE,
 	"payment_year" INT NOT NULL CHECK("payment_year" > 2000),
 	"jan" REAL NOT NULL DEFAULT 0,
@@ -109,26 +112,27 @@ CREATE TABLE "receipt" (
 	"oct" REAL NOT NULL DEFAULT 0,
 	"nov" REAL NOT NULL DEFAULT 0,
 	"dec" REAL NOT NULL DEFAULT 0,
-	"package_discount" REAL NOT NULL,
-	"reg_fees" REAL NOT NULL,
-	"incentive" REAL NOT NULL,
+	"package_discount" REAL NOT NULL DEFAULT 0,
+	"reg_fees" REAL NOT NULL DEFAULT 0,
+	"incentive" REAL NOT NULL DEFAULT 0,
 	"voucher_id" VARCHAR(255),
-	"voucher_discount" REAL NOT NULL,
+	"voucher_discount" REAL NOT NULL DEFAULT 0,
+	"voucher_is_percentage" BOOLEAN NOT NULL DEFAULT FALSE,
 	"tax" REAL NOT NULL,
+	"tax_inclusive" BOOLEAN NOT NULL,
 	"remarks" VARCHAR(255),
-	"status" VARCHAR(20) NOT NULL,
-	FOREIGN KEY ("student_id") REFERENCES "student"("id")
+	"status" VARCHAR(20),
 );
 
 CREATE TABLE "receipt_class" (
-	"receipt_id" INT,
-	"class_id" INT,
+	"receipt_id" INT NOT NULL,
+	"class_id" INT NOT NULL,
+	"class_name" VARCHAR(255) NOT NULL,
+	"teacher_id" INT NOT NULL,
+	"teacher_name" VARCHAR(255) NOT NULL,
 	"fees" REAL NOT NULL,
 	"is_package" BOOLEAN NOT NULL,
-	"package_discount" REAL NOT NULL,
 	PRIMARY KEY ("receipt_id", "class_id"),
-	FOREIGN KEY ("receipt_id") REFERENCES "receipt"("id"),
-	FOREIGN KEY ("class_id") REFERENCES "class_registration"("id")
 );
 
 CREATE TABLE "user" (
