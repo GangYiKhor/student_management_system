9e44061a93df5f21d1ea37641263b9e3daf4534a
Gang Yi <khorgangyi@gmail.com>
2024-09-19 20:06:38 +0100

    Added back button on layout component

--- renderer/components/header.tsx
+++ renderer/components/header.tsx
@@ -1,57 +1,22 @@
 import clsx from 'clsx';
-import { ButtonSmallClass } from '../utils/tailwindClass/button';
 import { ThemeToggle } from './theme-toggle';
 
 const headerClass = clsx(
-	'flex',
-	'justify-between',
-	'items-stretch',
-	'bg-slate-200',
-	'dark:bg-[rgb(33,37,41)]',
-	'p-5',
-	'select-none',
-	'border-b-2',
-	'border-white',
-	'dark:border-[rgba(255,255,255,0.1)]',
+	'flex justify-between items-center',
+	'bg-slate-200 dark:bg-[rgb(33,37,41)]',
+	'select-none p-5',
 );
-const titleClass = clsx(
-	'text-4xl',
-	'font-bold',
-	'underline',
-	'text-gray-700',
-	'dark:text-gray-300',
-);
-const buttonClass = clsx('flex', 'justify-end', 'items-center', 'gap-4');
+const titleClass = clsx('text-4xl font-bold underline', 'text-gray-700 dark:text-gray-300');
 
 type PropType = {
 	title: string;
-	buttons?: [
-		{
-			action: CallableFunction;
-			text: string;
-			buttonDesignClass: string;
-		},
-	];
 };
 
-export function Header({ title, buttons }: Readonly<PropType>) {
+export function Header({ title }: Readonly<PropType>) {
 	return (
 		<header className={headerClass}>
-			<div className={titleClass}>
-				<h1>{title}</h1>
-			</div>
-			<div className={buttonClass}>
-				{buttons?.map(({ action, text, buttonDesignClass }) => (
-					<button
-						key={text}
-						onClick={() => action()}
-						className={clsx(buttonDesignClass, ButtonSmallClass)}
-					>
-						{text}
-					</button>
-				))}
-				<ThemeToggle />
-			</div>
+			<h1 className={titleClass}>{title}</h1>
+			<ThemeToggle />
 		</header>
 	);
 }
--- renderer/layouts/basic_layout.tsx
+++ renderer/layouts/basic_layout.tsx
@@ -1,31 +1,39 @@
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
 import { Header } from '../components/header';
 import { NavigationBar } from '../components/navigation-bar';
 
-const layoutClass = clsx('flex', 'bg-[rgb(250,250,250)]', 'dark:bg-[rgb(42,46,50)]');
-
-const mainClass = clsx('flex-1', 'w-full', 'h-screen', 'overflow-auto');
-
+const layoutClass = clsx('flex', 'bg-[rgb(250,250,250)] dark:bg-[rgb(42,46,50)]');
+const mainClass = clsx('flex-1', 'w-full h-screen', 'overflow-auto');
 const contentClass = clsx('p-5');
+const backButtonClass = clsx(
+	'flex items-center gap-0.5',
+	'text-gray-600 dark:text-gray-400',
+	'hover:underline',
+	'active:opacity-80',
+	'pl-5 pt-3',
+);
 
 type PropType = {
-	children: any;
+	children: ReactNode;
 	headerTitle: string;
-	headerButtons?: [
-		{
-			action: CallableFunction;
-			text: string;
-			buttonDesignClass: string;
-		},
-	];
+	navigateBack?: boolean;
 };
 
-export function Layout({ children, headerTitle, headerButtons }: Readonly<PropType>) {
+export function Layout({ children, headerTitle, navigateBack }: Readonly<PropType>) {
+	const router = useRouter();
+
 	return (
 		<div className={layoutClass}>
 			<NavigationBar />
 			<main className={mainClass}>
-				<Header buttons={headerButtons} title={headerTitle} />
+				<Header title={headerTitle} />
+				{navigateBack ? (
+					<button className={backButtonClass} onClick={() => router.back()}>
+						{'< Back'}
+					</button>
+				) : null}
 				<section className={contentClass}>{children}</section>
 			</main>
 		</div>
